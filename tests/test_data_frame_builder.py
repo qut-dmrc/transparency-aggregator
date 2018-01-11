93a24c2309cad7df9aff47f8058377f552680e62
Tim Smith <tims@thoughtworks.com>
2018-01-11 10:23:20 +1000

    [1] (Tim, Jacob) added a second extract statement to test

--- tests/test_data_frame_builder.py
+++ tests/test_data_frame_builder.py
@@ -18,6 +18,9 @@ class TestDataFrameBuilder(unittest.TestCase):
 			'number requests' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
 			'number affected' : pd.Series([4, 5, 6], index=['a', 'b', 'c']),
 			'number complied' : pd.Series([7, 8, 9], index=['a', 'b', 'c']),
+			'another number requests' : pd.Series([11, 12, 13], index=['a', 'b', 'c']),
+			'another number affected' : pd.Series([14, 15, 16], index=['a', 'b', 'c']),
+			'another number complied' : pd.Series([17, 18, 19], index=['a', 'b', 'c']),
 			'country' : pd.Series(['aust', 'nz', 'us'], index=['a', 'b', 'c']),
 		}
 		df = pd.DataFrame(d)	
@@ -33,13 +36,22 @@ class TestDataFrameBuilder(unittest.TestCase):
 
 		cut = DataFrameBuilder(df_in, df_out, 'platform', 'platform_property', '2001-01-01', '2001-06-30')
 		cut.extract_columns('request_type', num_requests_col = 'number requests', num_affected_col = 'number affected', num_complied_col = 'number complied')
+		cut.extract_columns('another request_type', num_requests_col = 'another number requests', num_affected_col = 'another number affected', num_complied_col = 'another number complied')
 		new_df_out = cut.get_df()
 
-		self.assertEqual(3, len(new_df_out.index))
+		self.assertEqual(6, len(new_df_out.index))
+		self.assertEqual('aust', new_df_out['country'][0])
+		self.assertEqual('request_type', new_df_out['request_type'][0])
 		self.assertEqual(1, new_df_out['num_requests'][0])
 		self.assertEqual(4, new_df_out['num_affected'][0])
 		self.assertEqual(7, new_df_out['num_complied'][0])
-		self.assertEqual('aust', new_df_out['country'][0])
+
+		self.assertEqual('aust', new_df_out['country'][3])
+		self.assertEqual('another request_type', new_df_out['request_type'][3])
+		self.assertEqual(11, new_df_out['num_requests'][3])
+		self.assertEqual(14, new_df_out['num_affected'][3])
+		self.assertEqual(17, new_df_out['num_complied'][3])
+
 			
 
 if __name__ == '__main__':
