8741747c168a6e57e4153e28379d18d69c89fd8c
Jacob Olsen <jolsen@thoughtworks.com>
2018-01-16 15:24:24 +1000

    [25] (Jacob, Tim) Create SemiannualUrlSource

---
+++ semiannual_url_source.py
@@ -0,0 +1,45 @@
+""" 
+"""
+
+from source import Source
+from string import Template
+
+
+
+class SemiannualUrlSource(Source):
+    def get(self):
+
+        url_template = Template(self.config['url_template'])
+        start_year = int(self.config['start_year'])
+        end_year = int(self.config['end_year'])
+
+        data = []
+
+        for report_year in range(start_year, end_year + 1):
+
+            for i, substitutes in enumerate([
+                {
+                    'facebook_period': "H1",
+                    'start_month': "jan",
+                    'end_month': "jun",
+                },
+                {
+                    'facebook_period': "H2",
+                    'start_month': "jul",
+                    'end_month': "dec",
+                },
+            ]):
+                substitutes = {**substitutes, 'report_year': report_year}
+                url = url_template.substitute(substitutes)
+                if i == 0:
+                    start_date = "{}-01-01 00:00:00".format(report_year)
+                    end_date = "{}-06-30 23:59:59".format(report_year)
+                else:
+                    start_date = "{}-07-01 00:00:00".format(report_year)
+                    end_date = "{}-12-31 23:59:59".format(report_year)
+
+                period_data = {'url': url, 'report_start': start_date, 'report_end': end_date}
+
+                data.append(period_data)
+
+        return data
---
+++ tests/test_semiannual_url_source.py
@@ -0,0 +1,20 @@
+import unittest
+
+from semiannual_url_source import SemiannualUrlSource
+
+
+class TestSemiannualUrlSource(unittest.TestCase):
+    def setUp(self):
+        self.source = SemiannualUrlSource({"url_template": "https://transparency.example.com/$report_year-$facebook_period-$start_month-$end_month", 'start_year': '2016', 'end_year': '2017'})
+
+    def test_read_correctly_handles_numbers(self):
+        data = self.source.get()
+
+        expected = [
+            {"url": "https://transparency.example.com/2016-H1-jan-jun", "report_start": "2016-01-01 00:00:00", "report_end": "2016-06-30 23:59:59"},
+            {"url": "https://transparency.example.com/2016-H2-jul-dec", "report_start": "2016-07-01 00:00:00", "report_end": "2016-12-31 23:59:59"},
+            {"url": "https://transparency.example.com/2017-H1-jan-jun", "report_start": "2017-01-01 00:00:00", "report_end": "2017-06-30 23:59:59"},
+            {"url": "https://transparency.example.com/2017-H2-jul-dec", "report_start": "2017-07-01 00:00:00", "report_end": "2017-12-31 23:59:59"},
+        ]
+
+        self.assertEqual(expected, data)
