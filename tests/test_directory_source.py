2da8d3a0b985cd2c24ef23315a4ba2cd8c8d540c
Tim Smith <tims@thoughtworks.com>
2018-01-24 15:19:32 +1000

    [X] (Tim, Jacob) Update test to work on non-project-root paths

--- tests/test_directory_source.py
+++ tests/test_directory_source.py
@@ -1,11 +1,12 @@
 from tests.transparency_test_case import TransparencyTestCase
+from transparency import utils
 from transparency.directory_source import DirectorySource
 
 
 class TestDirectorySource(TransparencyTestCase):
     def setUp(self):
         self.source = DirectorySource(
-            {"directory": "tests/files/manual/"}
+            {"directory": utils.make_path("tests/files/manual/")}
         )
 
     def test_read_correctly_handles_numbers(self):
